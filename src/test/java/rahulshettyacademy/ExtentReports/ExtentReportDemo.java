15770f72971d61fa6215f8f2ccc5682933757119
rumana <rumanashaikhbade123@gmail.com>
2024-10-10 14:40:56 +0530

    first commit

---
+++ src/test/java/rahulshettyacademy/ExtentReports/ExtentReportDemo.java
@@ -0,0 +1,18 @@
+package rahulshettyacademy.ExtentReports;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ExtentReportDemo {
+
+	
+	
+	@Test
+	public void initialDemo()
+	{
+		System.setProperty("webdriver.chrome.driver","C://chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		
+	}
+}
